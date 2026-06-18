16428abdfddbb4bc8dcd5f4dcce4f5e5f4cfab35
MuhammadDuryabBakht <itsduryab@gmail.com>
2026-06-18 22:46:38 +0500

    Calculator installed

---
+++ calculator.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cout<<"Enter 1st number: ";
+    cin>>x;
+    cout<<"Enter 2st number: ";
+    cin>>y;
+    char op;
+    cout<<"Enter Arithmetic opreation you want to perfrom: "<<endl;
+    cin>>op;
+    switch(op){
+        case('+'):
+        cout<<"Addition of "<<x<<" and "<<y<<" is: "<<x+y;
+        break;
+        case('-'):
+        cout<<"Subtraction of "<<x<<" and "<<y<<" is: "<<x-y;
+        break;
+        case('*'):
+        cout<<"Product of "<<x<<" and "<<y<<" is: "<<x*y;
+        break;
+        case('/'):
+        cout<<"Division of "<<x<<" and "<<y<<" is: "<<x/y;
+        break;
+    }
+    return 0;
+}
